7972725e0ac06af91cc8ce690dee487ef90150b2
Aphaki <kiapha1108@gmail.com>
2023-06-04 03:19:47 +0900

    Initial Commit

---
+++ LOL_Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LOL_Search
+//
+//  Created by Sy Lee on 2023/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
