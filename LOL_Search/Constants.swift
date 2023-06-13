7a2d7e529c3c0c445be6cbf1f55b893f02192ca9
Aphaki <kiapha1108@gmail.com>
2023-06-13 17:45:57 +0900

    ??

--- LOL_Search/Constants.swift
+++ LOL_Search/Constants.swift
@@ -11,4 +11,5 @@ enum Constants {
     static let X_Riot_Token: String = "RGAPI-308b567c-25e4-47ce-890d-0c6c1972311d"
     
     static let nowTime: TimeInterval = Date().timeIntervalSince1970
+    // ??
 }
