3b92899e2a5afd2fc06ba77f71226aced31378a6
Aphaki <kiapha1108@gmail.com>
2023-06-14 19:02:50 +0900

    Add error method

--- LOL_Search/View Model/MainVM.swift
+++ LOL_Search/View Model/MainVM.swift
@@ -6,28 +6,38 @@
 //
 
 import Foundation
-import RxSwift
-import RxRelay
+import Combine
 
 class MainVM {
     
     let service = Service()
     
+    @Published var noSummonerErrorAlert = PassthroughSubject<(),Never>()
     private (set)var urlBase: UrlHeadPoint = .kr
     private (set)var searchedSummonerDetails: [DetailSummonerInfo] = []
+    
+    private var cancellable = Set<AnyCancellable>()
     
     init() {
         subscribe()
     }
     
     private func subscribe() {
-        
+        // 소환사 검색 - 소환사 없음 에러
+        NetworkManager.shared.$noSummonerError
+            .sink { completion in
+                print("MainVM - noSummonerErrorAlert subscribe completion ")
+            } receiveValue: { [weak self] value in
+                guard let self = self else { return }
+                self.noSummonerErrorAlert = value
+            }
+            .store(in: &cancellable)
     }
     
     func searchSummonersInfo(name: String) {
         Task {
-            let value = try await service.saveSearchedSummonerDetail(urlBase: urlBase, name: name)
-            self.searchedSummonerDetails.append(value)
+            let aSearchedSummonerDetail = try await service.saveSearchedSummonerDetail(urlBase: urlBase, name: name)
+            self.searchedSummonerDetails.append(aSearchedSummonerDetail)
         }
     }
     
